b822f14db33590c67c3313d29c7c1ed6914a40f9
Brandon Ho <146787216+brandonvho@users.noreply.github.com>
2026-05-10 14:35:46 -0500

    Delete website directory

--- website/src/settings.ts
+++
@@ -1,5 +0,0 @@
-export const transition = {
-  type: "spring" as const,
-  duration: 0.7,
-  bounce: 0.2
-};
--- website/src/use-smooth-transform.tsx
+++
@@ -1,9 +0,0 @@
-import { useSpring, useTransform, MotionValue, type SpringOptions } from "framer-motion";
-
-export function useSmoothTransform(
-  value: MotionValue<number>,
-  springOptions: SpringOptions,
-  transformer: (value: number) => number
-): MotionValue<number> {
-  return useSpring(useTransform(value, transformer), springOptions);
-}
